e51cfbc737d85cc4faf68fd20fcf011edf6d1bf5
4260snow <75683039+4260snow@users.noreply.github.com>
2022-08-31 13:35:27 +0300

    Update SparseMatrix.h

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -6,27 +6,26 @@
 
 class SparseMatrix : public IMatrix, public LinkedList
 {
-    private:
-        int row;
-        int col;
-        int count;
-        LinkedList *data;
-    public:
-        SparseMatrix();
-        SparseMatrix(const SparseMatrix &m);
-        SparseMatrix(int row, int col); //Матрица заполненная rand до row и до col
-        SparseMatrix(int row, int col, int value); //Матрица заполненная value  до row и до col
+	private:
+		int row;
+		int col;
+		int count;
+		LinkedList *data;
+	public:
+		SparseMatrix();
+		SparseMatrix(const SparseMatrix &m);
+		SparseMatrix(int row, int col); //Матрица заполненная rand до row и до col
+		SparseMatrix(int row, int col, int value); //Матрица заполненная value  до row и до col
 
-        void setElem(int x, int y, int value);
-        int getElem(int x, int y);
-        int getSizeRow();
-        int getSizeCol();   
-        int getSize();
-        void print();
+		void setElem(int x, int y, int value);
+		int getElem(int x, int y);
+		int getSizeRow();
+		int getSizeCol();   
+		int getSize();
+		void print();
 
-        SparseMatrix& operator=(const SparseMatrix &m);
-        ~SparseMatrix();
-
+		SparseMatrix& operator=(const SparseMatrix &m);
+		~SparseMatrix();
 };
 
 #endif
